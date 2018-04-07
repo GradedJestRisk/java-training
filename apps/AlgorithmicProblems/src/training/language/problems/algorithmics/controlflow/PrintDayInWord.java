da6b399794f2fd4e6c05d3831ac6d395003b1eed
GradedJestRisk <graded.jest.risk@gmail.com>
2018-04-07 19:25:34 +0200

    Add IDEAJ project with some problems

---
+++ apps/AlgorithmicProblems/src/training/language/problems/algorithmics/controlflow/PrintDayInWord.java
@@ -0,0 +1,100 @@
+package training.language.problems.algorithmics.controlflow;
+
+class PrintDayInWord {
+
+  private static final String FIRST_DAY   = "monday";
+  private static final String SECOND_DAY  = "tuesday";
+  private static final String THIRD_DAY   = "wednesday";
+  private static final String FOURTH_DAY  = "thursday";
+  private static final String FIFTH_DAY   = "friday";
+  private static final String SIXTH_DAY   = "saturday";
+  private static final String SEVENTH_DAY = "sunday";
+  private static final String UNKNOWN_DAY = "unknow";
+
+  //  no constructor
+
+  void execute() {
+
+    String DayInWord;
+
+    System.out.println("Using if");
+
+    for (int i = 1; i <= 8; i++) {
+
+      if (i == 1) {
+        DayInWord = FIRST_DAY;
+      } else if (i == 2) {
+        DayInWord = SECOND_DAY;
+      } else if (i == 3) {
+        DayInWord = THIRD_DAY;
+      } else if (i == 4) {
+        DayInWord = FOURTH_DAY;
+      } else if (i == 5) {
+        DayInWord = FIFTH_DAY;
+      } else if (i == 6) {
+        DayInWord = SIXTH_DAY;
+      } else if (i == 7) {
+        DayInWord = SEVENTH_DAY;
+      } else {
+        DayInWord = UNKNOWN_DAY;
+      }
+
+      if (! DayInWord.equals(UNKNOWN_DAY)) {
+        System.out.println("Day " + i + " is: " + DayInWord);
+      }
+      else {
+        System.out.println("Day " + i + " is not a valid day");
+      }
+
+    }
+
+    System.out.println("Using switch");
+
+    for (int i = 1; i <= 8; i++) {
+
+      switch (i) {
+
+        case 1:
+          DayInWord = FIRST_DAY;
+          break;
+
+        case 2:
+          DayInWord = SECOND_DAY;
+          break;
+
+        case 3:
+          DayInWord = THIRD_DAY;
+          break;
+
+        case 4:
+          DayInWord = FOURTH_DAY;
+          break;
+
+        case 5:
+          DayInWord = FIFTH_DAY;
+          break;
+
+        case 6:
+          DayInWord = SIXTH_DAY;
+          break;
+
+        case 7:
+          DayInWord = SEVENTH_DAY;
+          break;
+
+        default:
+          DayInWord = UNKNOWN_DAY;
+      }
+
+      if (! DayInWord.equals(UNKNOWN_DAY)) {
+        System.out.println("Day " + i + " is: " + DayInWord);
+      }
+      else {
+        System.out.println("Day " + i + " is not a valid day");
+      }
+
+    }
+
+  }
+
+}
---
+++ apps/AlgorithmicProblems/src/training/language/problems/algorithmics/iteration/Main.java
@@ -0,0 +1,16 @@
+package training.language.problems.algorithmics.iteration;
+
+
+public class Main {
+
+  public static void main(String[] args) {  // Program entry point
+
+    int sum_start = 1;
+    int sum_end = 3;
+
+    SumAndAverage test = new SumAndAverage(sum_start, sum_end);
+    test.execute();
+
+  }
+
+}
