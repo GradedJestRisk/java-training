ad1356d35bf175f0ef6dc1d94f815c68e7f19145
GradedJestRisk <graded.jest.risk@gmail.com>
2025-04-03 17:35:39 +0200

    Add packaging

--- packaging/maven/exclude-in-artifact/src/main/java/com/example/exclude_in_artifact/ExcludeInArtifactApplication.java
+++ packaging/maven/exclude-in-artifact/src/main/java/com/example/exclude_in_artifact/ExcludeInArtifactApplication.java
@@ -1,14 +1,13 @@
-package com.example.monitoring;
+package com.example.exclude_in_artifact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ExcludeInArtifactApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ExcludeInArtifactApplication.class, args);
 	}
 
 }
---
+++ packaging/maven/exclude-in-artifact/src/test/java/com/example/exclude_in_artifact/ExcludeInArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exclude_in_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcludeInArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
